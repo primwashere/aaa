5c2213c15828b5b952f21334cfb83abdcf7a5161
Aleksandra Wojewoda <a.wojewoda18@gmail18.com>
2023-03-31 10:22:12 +0200

    Correct def

--- ex_1.py
+++ ex_1.py
@@ -1,3 +1,12 @@
-def hello(name):
-    print("Hello " + name)
+def hello(a: str) -> str:
+    # TODO
+    return "Hello " + a
 
+
+print("Example")
+print(hello("Ola"))
+
+assert hello("Ola") == "Hello Ola"
+assert hello("Zuzia") == "Hello Zuzia"
+
+print("Zadanie zrobione")
