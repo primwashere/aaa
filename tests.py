9ab8561e74b0818025e142c85584381c9c2f6529
Aleksandra Wojewoda <aleksandra.wojewoda@ue.poznan.pl>
2023-04-23 20:57:43 +0200

    Fix some tests

--- tests.py
+++ tests.py
@@ -2,16 +2,18 @@ from ex_1 import *
 from unittest.mock import patch
 import unittest
 
+
 @patch('builtins.print')
 def test_hello(mock_print):
     hello("Ola")
-    mock_print.assert_called_with("Hello Tesss")
+    mock_print.assert_called_with("Hello Ola")
     hello("Zuzia")
     mock_print.assert_called_with("Hello Zuzia")
 
     import sys
-    sys.stdout.write(str( mock_print.call_args ) + '\n')
-    sys.stdout.write(str( mock_print.call_args_list ) + '\n')
+    sys.stdout.write(str(mock_print.call_args) + '\n')
+    sys.stdout.write(str(mock_print.call_args_list) + '\n')
+
 
 if __name__ == '__main__':
     test_hello()
