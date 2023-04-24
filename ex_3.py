781755b3d82f675aeb323d99aa64d722d6ee93d2
Ola Hamrol <ola.hamrol@gmail.com>
2023-04-24 15:56:22 +0200

    Ola's changes ex_3

--- ex_3.py
+++ ex_3.py
@@ -5,12 +5,13 @@
 def policz_studentow_plec(studenci) -> [int, int]:
     liczba_kobiet = 0
     liczba_mezczyzn = 0
-    for imie in studenci:
-        if isinstance(imie, str):
+    for dane in studenci:
+        if isinstance(dane, str):
+            imie = dane.split()[0]
             if imie[-1] == 'a':
                 liczba_kobiet += 1
             else:
                 liczba_mezczyzn += 1
     return [liczba_mezczyzn, liczba_kobiet]
 
-# print(policz_studentow_plec([17, 12, "Ola Hamrol", "Ania Oleksy", "Piotr Kawa", "Basia Kwiatkowski", "Marek Rydz"]))
+print(policz_studentow_plec([17, 12, "Ola Hamrol", "Ania Oleksy", "Piotr Kawa", "Basia Kwiatkowski", "Marek Rydz"]))
